2fe0947305da29914246cd47e9faf75811f83608
pranaykrpiyush <pkp5214@gmail.com>
2020-03-31 10:44:59 +0530

    Finished

---
+++ script.js
@@ -0,0 +1,7 @@
+function expand(idName){
+    document.querySelector('#'+ idName).classList.add('reveal');
+}
+//onclick= "expand('Education')"
+//document.querySelector('#expands').addEventListener('click',function(){
+  //  expand('expands')
+//});
